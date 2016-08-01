ae88dbb1752461058f0990a0c82cafa950d65b98
Davide Ghezzi <davideghz@gmail.com>
2016-07-31 20:02:52 -0400

    display API result - second attempt

--- config/initializers/mixpanel.rb
+++ config/initializers/mixpanel.rb
@@ -9,6 +9,6 @@ if Rails.env.development?
 end
 
 $mixpanel_client = Mixpanel::Client.new(
-    api_key:    'ENV['MIXPANEL_API_KEY']',
-    api_secret: 'ENV['MIXPANEL_API_SECRET']'
+    api_key:    ENV['MIXPANEL_API_KEY'],
+    api_secret: ENV['MIXPANEL_API_SECRET']
 )
